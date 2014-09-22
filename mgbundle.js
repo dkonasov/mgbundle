309d6d78abf17cd4f058bb323f1c036d183d57e6
dkonasov <d.konasov@yandex.ru>
2014-09-22 14:40:45 +0400

    Added simpleRender method for black-and-white graphics

--- mgbundle.js
+++ mgbundle.js
@@ -4,9 +4,25 @@ this.pixelsPerSprite=pixelsPerSprite;
 this.spriteX=spriteX;
 this.spriteY=spriteY;
 this.canvasSelector=canvasSelector;
-this.render=function(camera, field){
+this.canvas=document.querySelector(canvasSelector);
+this.canvas.width=pixelsPerSprite*spriteX;
+this.canvas.height=pixelsPerSprite*spriteY;
+this.ctx=this.canvas.getContext('2d');
+this.simpleRender=function(camera, field){
+for(var i=camera.y; i<camera.y+this.spriteY; i++){
 
-console.log('rendering');
+for(var j=camera.x; j<camera.x+this.spriteX; j++){
+
+if(field[i][j]==1){
+
+this.ctx.fillRect(j*this.pixelsPerSprite, i*this.pixelsPerSprite, this.pixelsPerSprite, this.pixelsPerSprite);
+
+}
+
+}
+
+}
+
 
 }
 console.log('Initialized MGCanvas on '+canvasSelector+' with resolution '+pixelsPerSprite*spriteX+'X'+pixelsPerSprite*spriteY);
